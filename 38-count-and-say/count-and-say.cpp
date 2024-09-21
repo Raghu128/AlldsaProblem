ddd2c31f19e24e0cd5e0641f882342dc5bfab757
Raghu128 <119107622+Raghu128@users.noreply.github.com>
2024-09-21 21:06:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (86.31%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string countAndSay(int n) {
+    string solve(int n) {
         if(n == 1) return "1";
 
         string subAns = countAndSay(n-1);
@@ -19,10 +19,43 @@ public:
                 cnt = 1;
             }
         }
-
         ans += char('0' + cnt);
         ans += ch;
 
         return ans;
+    }
+    string solveTab(int size) {
+        unordered_map<int,string> dp;
+        dp[1] = "1";
+        
+        for(int n = 2; n <= size; n++) {
+            string subAns = dp[n-1];
+            string ans = "";
+
+            char ch = subAns[0];
+            int cnt = 0;
+            for(auto i : subAns) {
+                if(i == ch) {
+                    cnt++;
+                }
+                else {
+                    ans += char('0' + cnt);
+                    ans += ch;
+                    ch = i;
+                    cnt = 1;
+                }
+            }
+            ans += char('0' + cnt);
+            ans += ch;
+
+            // cout << subAns << " " << ans << endl;
+            dp[n] = ans;
+        }
+
+        return dp[size];
+    }
+    string countAndSay(int n) {
+        return solveTab(n);
+        // return solve(n);
     }
 };
