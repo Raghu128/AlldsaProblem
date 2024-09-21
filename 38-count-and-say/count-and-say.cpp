f08794581dd6e3310d5e4918661bad1d38fbdda2
Raghu128 <119107622+Raghu128@users.noreply.github.com>
2024-09-21 21:09:37 +0530

    Time: 5 ms (51.70%) | Memory: 8.4 MB (92.75%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -54,8 +54,41 @@ public:
 
         return dp[size];
     }
+
+    string solveTabOpt(int size) {
+        if(size == 1) return "1";
+
+        // unordered_map<int,string> dp;
+        string s = "1";
+        
+        for(int n = 2; n <= size; n++) {
+            string subAns = s;
+            string ans = "";
+
+            char ch = subAns[0];
+            int cnt = 0;
+            for(auto i : subAns) {
+                if(i == ch) {
+                    cnt++;
+                }
+                else {
+                    ans += char('0' + cnt);
+                    ans += ch;
+                    ch = i;
+                    cnt = 1;
+                }
+            }
+            ans += char('0' + cnt);
+            ans += ch;
+
+            s = ans;
+        }
+
+        return s;
+    }
     string countAndSay(int n) {
-        return solveTab(n);
+        return solveTabOpt(n);
+        // return solveTab(n);
         // return solve(n);
     }
 };
