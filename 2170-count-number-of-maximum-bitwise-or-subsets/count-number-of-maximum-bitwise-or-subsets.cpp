17a083990fb4813dde2ff01b844946a113ac7fb2
Raghu Kumar <119107622+Raghu128@users.noreply.github.com>
2024-10-18 20:29:20 +0530

    Time: 131 ms (11.99%) | Memory: 312.5 MB (6.76%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    unordered_map<int,int> mp;
-    void solve(int i, int val, vector<int>& a, int n) {
+    int solve(int i, int currVal, int tar, vector<int>& a, int n, vector<vector<int>> &dp) {
         if(i == n) {
-            mp[val]++;
-            return;
+            if(currVal == tar) return 1;
+            return 0;
         }
+        if(dp[i][currVal] != -1) return dp[i][currVal];
 
-       solve(i+1, val | a[i], a, n);
-       solve(i+1, val, a, n);
+        int take = solve(i+1, currVal | a[i], tar, a, n, dp);
+        int notTake = solve(i+1, currVal, tar, a, n, dp);
 
-       return ;
+        return dp[i][currVal] = take + notTake;
     }
     int countMaxOrSubsets(vector<int>& nums) {
         int n = nums.size();
-        solve(0, 0, nums, n);
-
         int maxi = 0;
-        for(auto i : mp) maxi = max(maxi, i.first);
 
-        return mp[maxi];
+        for(auto i : nums) maxi |= i;
+
+        vector<vector<int>> dp(n+1, vector<int>(maxi+1, -1));
+        return solve(0, 0, maxi, nums, n, dp);
+
+
     }
 };
