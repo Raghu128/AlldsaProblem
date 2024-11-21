a85aaf4092fd20b60d235b512aaee76ea856813d
Raghu Kumar <119107622+Raghu128@users.noreply.github.com>
2024-11-21 23:38:07 +0530

    Time: 29 ms (77.37%) | Memory: 94.8 MB (58.70%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.cpp
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     void dfs(int i, int j, vector<vector<int>>& grid1, vector<vector<int>>& grid2, int n, int m, bool& flag) {
-        // cout << i << " " << j << endl;
-        if(i < 0 || i == n || j < 0 || j == m || grid2[i][j] == 0) return ;
+        if(i < 0 || i == n || j < 0 || j == m || grid2[i][j] == 0 || grid2[i][j] == -1) return ;
         if(grid1[i][j] == 1 && grid2[i][j] == 0) return ;
 
         if(grid1[i][j] == 0 && grid2[i][j] == 1) {
             flag = false;
         }
 
-        grid2[i][j] = 0;
+        grid2[i][j] = -1;
 
         dfs(i, j+1, grid1, grid2, n, m, flag);
         dfs(i, j-1, grid1, grid2, n, m, flag);
