369bcd3edd86bc5a34162b870d7b5373ef66f196
Raghu Kumar <119107622+Raghu128@users.noreply.github.com>
2024-11-21 17:32:20 +0530

    Time: 30 ms (33.35%) | Memory: 72.8 MB (19.32%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -3,12 +3,13 @@ public:
     int findJudge(int n, vector<vector<int>>& trust) {
         vector<int> freq(n+1, 0);
 
-        for(auto i : trust) freq[i[1]]++;
+        for(auto i : trust) {
+            freq[i[1]]++;
+            freq[i[0]]--;
+        }
 
         int ans = -1;
         for(int i = 1; i <= n; i++) if(freq[i] == n-1) ans = i;
-
-        for(auto i : trust) if(i[0] == ans) return -1;
 
         return ans;
     }
