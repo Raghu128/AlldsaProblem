914e49dea469cc4c5e8e29911c483e8aa2e7c7fe
Raghu Kumar <119107622+Raghu128@users.noreply.github.com>
2024-12-05 21:07:17 +0530

    Time: 4 ms (90.23%) | Memory: 21.9 MB (33.33%) - LeetSync

---
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canChange(string start, string target) {
+        int waitL = 0, waitR = 0;
+        int n = start.size();
+
+        for(int i = 0; i < n; i++) {
+            char curr = start[i], goal = target[i];
+            
+            if(goal == 'L') {
+                if(waitR > 0) return false;
+                waitL++;
+            }
+            if(curr == 'R') {
+                if(waitL > 0) return false;
+                waitR++;
+            }
+            if(goal == 'R') {
+                if(waitR == 0) return false;
+                waitR--;
+            }
+            if(curr == 'L') {
+                if(waitL == 0) return false;
+                waitL--;
+            }
+        }
+
+        if(waitR == 0 && waitL == 0) return true;
+        return false;
+    }
+};
