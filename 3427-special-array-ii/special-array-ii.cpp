03502ddb9a3c6f4dcd340ae40a42e43f814a7a6b
Raghu Kumar <119107622+Raghu128@users.noreply.github.com>
2024-12-09 22:02:27 +0530

    Time: 42 ms (37.89%) | Memory: 136.3 MB (25.40%) - LeetSync

---
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<bool> ans;
+        int n = nums.size();
+        vector<int> prefix(n, 0);
+
+        for(int i = 1; i < n; i++) {
+            prefix[i] = prefix[i-1];
+            if ((nums[i - 1] % 2 == 0 && nums[i] % 2 == 0) || (nums[i - 1] % 2 != 0 && nums[i] % 2 != 0)) {
+                prefix[i]++;
+            }
+        }
+
+        for(auto i : queries) {
+            int start = i[0], last = i[1];
+            int total = prefix[last];
+            if(start != 0) total -= prefix[start];
+
+            if(total == 0) ans.push_back(true);
+            else ans.push_back(false);
+        }
+
+        return ans;
+    }
+};
