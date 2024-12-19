a5dcfb0d281a2bd2ce1bab4e1fa5a597e5e08398
Raghu Kumar <119107622+Raghu128@users.noreply.github.com>
2024-12-19 22:56:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (35.22%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -3,19 +3,17 @@ public:
     int maxChunksToSorted(vector<int>& arr) {
         int n = arr.size();
         int idx = 0, ans = 0;
-        vector< pair<int,int> > num;
+        vector< int > num(n);
 
-        for(int i = 0; i < n; i++) num.push_back({arr[i], i});
-        sort(num.begin(), num.end());
+        for(int i = 0; i < n; i++) num[arr[i]] = i;
 
         for(int i = 0; i < n; i++) {
             if(idx > i) {
-                idx = max(idx, num[i].second+1);
+                idx = max(idx, num[i]+1);
             }
             else {
                 ans++;
-                idx = num[i].second+1;
-                // cout << idx << endl;
+                idx = num[i]+1;
             }
         }
 
