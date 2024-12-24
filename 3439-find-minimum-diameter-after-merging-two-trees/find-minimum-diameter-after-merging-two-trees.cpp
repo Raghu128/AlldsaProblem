d8eaf7434cb2c35b193a3b5f0138eda98e345d58
Raghu Kumar <119107622+Raghu128@users.noreply.github.com>
2024-12-24 22:32:50 +0530

    Time: 643 ms (22.74%) | Memory: 395.5 MB (13.40%) - LeetSync

---
+++ 3439-find-minimum-diameter-after-merging-two-trees/find-minimum-diameter-after-merging-two-trees.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int diameter(int parent, int node, unordered_map<int,int>& dis, unordered_map<int,vector<int>>& adj) {
+        if(dis.find(node) != dis.end()) return dis[node];
+
+        int ans = 0;
+        for(auto i : adj[node]) {
+            if(parent != i)
+                ans = max(ans, 1+diameter(node, i, dis, adj));
+        }
+        dis[node] = ans;
+        return ans;
+    }
+    pair<int,int> findFarNode(int cnt, int node, int parent, unordered_map<int,vector<int>>& adj) {
+        // if(adj[node].size() == 1) return {cnt, node};
+
+        pair<int,int> ans = {cnt, node};
+        for(auto i : adj[node]) {
+            if(parent != i) {
+                pair<int,int> call = findFarNode(cnt+1, i, node, adj);
+                if(ans.first < call.first) ans = call;
+            }
+        }
+
+        return ans;
+    }
+    int minimumDiameterAfterMerge(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
+        int n = edges1.size(), m = edges2.size();
+        unordered_map<int,vector<int>> adj1, adj2;
+
+        for(int i = 0; i < n; i++) {
+            int u = edges1[i][0], v = edges1[i][1];
+            adj1[u].push_back(v);
+            adj1[v].push_back(u);
+        }
+        for(int i = 0; i < m; i++) {
+            int u = edges2[i][0], v = edges2[i][1];
+            adj2[u].push_back(v);
+            adj2[v].push_back(u);
+        }
+
+        int dia1 = 0, dia2 = 0;
+        unordered_map<int,int> dis1, dis2;
+        pair<int,int> tree1 = findFarNode(0, 0, -1, adj1);
+        pair<int,int> tree2 = findFarNode(0, 0, -1, adj2);
+
+        dia1 = diameter(-1, tree1.second,  dis1, adj1);
+        dia2 = diameter(-1, tree2.second,  dis2, adj2);
+
+        if(n == 0 && m == 0) return 1;
+        int d1 = dia1, d2 = dia2;
+        if(dia1%2) dia1++;
+        if(dia2%2) dia2++;
+        if(n == 0) return dia2;
+        if(m == 0) return dia1;
+
+        int ans = 1 + dia1/2 + dia2/2;
+
+        return max(d1, max(d2, ans));
+    }
+};
