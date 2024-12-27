5fc85673697af68a7352195c0644d8368c1bb6cd
Raghu Kumar <119107622+Raghu128@users.noreply.github.com>
2024-12-27 20:14:08 +0530

    Time: 55 ms (43.15%) | Memory: 47 MB (29.57%) - LeetSync

---
+++ 721-accounts-merge/accounts-merge.cpp
@@ -0,0 +1,90 @@
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        int n = accounts.size();
+        unordered_map<string,int> mp;
+        DisjointSet ds(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < accounts[i].size(); j++) {
+                string mail = accounts[i][j];
+                if (mp.find(mail) != mp.end()) {
+                    ds.unionBySize(i, mp[mail]);
+                } 
+                else mp[mail] = i;
+            }
+        }
+
+        unordered_map<int, set<string>> storage;
+        for(int i = 0; i < n; i++) {
+            int parent = ds.findUPar(i);
+            
+            for(int j = 1; j <  accounts[i].size(); j++) storage[parent].insert(accounts[i][j]);
+        }
+        vector<vector<string>> ans;
+        for(auto i : storage) {
+            int idx = i.first;
+            vector<string> sub;
+            sub.push_back(accounts[idx][0]);
+
+            for(auto j : i.second) sub.push_back(j);
+            ans.push_back(sub);
+        }
+
+        sort(ans.begin(), ans.end());
+        
+        return ans;
+
+    }
+};
