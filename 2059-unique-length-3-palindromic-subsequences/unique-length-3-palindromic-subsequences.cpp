c25dda5add8ba9642ce9c2caa17bf6971819eecc
Raghu Kumar <119107622+Raghu128@users.noreply.github.com>
2025-01-04 13:27:03 +0530

    Time: 159 ms (70.55%) | Memory: 16 MB (56.14%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        vector<int> first = vector(26, -1);
-        vector<int> last = vector(26, -1);
-        
-        for (int i = 0; i < s.size(); i++) {
-            int curr = s[i] - 'a';
-            if (first[curr] == - 1) {
-                first[curr] = i;
-            }
-            
-            last[curr] = i;
+        int n = s.size();
+        vector<int> left(26, -1), right(26, -1);
+
+        for(int i = 0; i < n; i++) {
+            int idx = s[i]-'a';
+
+            if(left[idx] == -1) left[idx] = i;
+            right[idx] = i;
         }
-        
+
+
         int ans = 0;
-        for (int i = 0; i < 26; i++) {
-            if (first[i] == -1) {
-                continue;
+        for(int i = 0; i < 26; i++) {
+            if(left[i] != -1 && right[i] != -1) {
+                int start = left[i], end = right[i], cnt = 0;
+
+                unordered_set<char> st;
+                for(int it = start+1; it < end; it++) st.insert(s[it]);
+                ans += st.size();
             }
-            
-            unordered_set<char> between;
-            for (int j = first[i] + 1; j < last[i]; j++) {
-                between.insert(s[j]);
-            }
-            
-            ans += between.size();
         }
-        
+
         return ans;
     }
 };
