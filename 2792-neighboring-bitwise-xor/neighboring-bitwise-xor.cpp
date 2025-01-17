6bc901448ecddd3f06b2bbfe7717e7b03b42425d
Raghu Kumar <119107622+Raghu128@users.noreply.github.com>
2025-01-17 16:18:30 +0530

    Time: 3 ms (59.81%) | Memory: 263.8 MB (74.77%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -1,51 +1,9 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
-        int n = derived.size();
-        if(n == 1) return !derived[0];
-        if(n == 2) return derived[0] == derived[1];
+        int cnt = 0;
+        for(auto i : derived) cnt += i;
 
-        vector<int> arr(n, 0);
-        bool flag = true;
-        for(int i = 0; i < n; i++) {
-            int nxtIdx = (i+1)%n;
-            int curr = arr[i];
-            int wants = derived[i];
-
-            if(nxtIdx == 0 && (curr ^ arr[0]) != wants) {
-                flag = false;
-                break;
-            }
-            else if(curr^1 == wants) arr[nxtIdx] = 1;
-            else if(curr^0 == wants) arr[nxtIdx] = 0;
-            else {
-                flag = false;
-                break;
-            }
-        }
-
-        if(!flag) {
-            arr[0] = 1;
-            for(int i = 0; i < n; i++) {
-                int nxtIdx = (i+1)%n;
-                int curr = arr[i];
-                int wants = derived[i];
-
-                if(nxtIdx == 0 ) {
-                    if( (curr ^ arr[0]) != wants) return false;
-                    else return true;
-                }
-
-                if(curr^1 == wants) arr[nxtIdx] = 1;
-                else if(curr^0 == wants) arr[nxtIdx] = 0;
-                else {
-                    return false;
-                }
-
-            }
-        }
-        
-        return true;
-
+        return cnt%2 == 0;
     }
 };
